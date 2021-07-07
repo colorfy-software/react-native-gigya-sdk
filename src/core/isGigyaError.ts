5120f8f751bf1ce555c1a6dbf249d1784879cb56
Charles Mangwa <charlesmangwa@gmail.com>
2021-07-07 14:34:34 +0200

    fix: allow isGigyaError() to handle cases where errorDetails isn't provided

--- src/core/isGigyaError.ts
+++ src/core/isGigyaError.ts
@@ -4,8 +4,7 @@ export default function (error?: GigyaSdkErrorType): Promise<boolean> {
   return new Promise(async (resolve) => {
     if (
       (error?.code || error?.payload?.errorCode) &&
-      error?.payload?.errorDetails &&
-      error?.payload?.errorMessage
+      (error?.payload?.errorDetails || error?.payload?.errorMessage)
     ) {
       return resolve(true)
     }
