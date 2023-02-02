16cf467cb4d56b21cb009a4b00c455b598fc6608
Charles Mangwa <charles@colorfy.me>
2023-02-02 11:59:18 +0100

    fix: update isGigyaError() check

--- src/core/isGigyaError.ts
+++ src/core/isGigyaError.ts
@@ -2,10 +2,7 @@ import type { GigyaSdkErrorType } from '../types'
 
 export default function (error?: GigyaSdkErrorType): Promise<boolean> {
   return new Promise(async (resolve) => {
-    if (
-      (error?.code || error?.payload?.errorCode) &&
-      (error?.payload?.errorDetails || error?.payload?.errorMessage)
-    ) {
+    if (error?.code && (error?.payload?.errorDetails || error?.payload?.errorMessage || error?.payload?.errorCode)) {
       return resolve(true)
     }
     resolve(false)
