3641093f8d1e891c7a89c7f340ea65a368059593
Charles Mangwa <charles@colorfy.me>
2024-08-20 08:37:33 +0200

    feat: add support for new consent API to `getRequiredConsentsSchema()`
    New consents API: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/1f1f6159d48a42bf9168c3d5567f1acd.html.

--- src/core/getRequiredConsentSchemas.ts
+++ src/core/getRequiredConsentSchemas.ts
@@ -1,26 +1,28 @@
-import type { GigyaConsentSchemaType } from '../types'
+import type { GigyaSdkConsentSchemaType, GigyaSdkConsentsStatementsType } from '../types'
 
 import sendApiCall from './sendApiCall'
 
 type PartialGigyaSchema = {
   preferencesSchema: {
-    fields: Record<string, GigyaConsentSchemaType>
+    fields: Record<string, GigyaSdkConsentSchemaType>
   }
 }
 
-export default function (): Promise<GigyaConsentSchemaType[]> {
+export default function (): Promise<GigyaSdkConsentSchemaType[]> {
   return new Promise(async (resolve, reject) => {
     try {
       const completeGigyaSchema = await sendApiCall<PartialGigyaSchema>('accounts.getSchema')
-      let output: GigyaConsentSchemaType[] = []
+      const currentSiteConsents = await sendApiCall<GigyaSdkConsentsStatementsType>('accounts.getConsentsStatements')
 
-      for (let key in completeGigyaSchema?.preferencesSchema?.fields) {
-        const consentSchema: GigyaConsentSchemaType = completeGigyaSchema.preferencesSchema.fields[key]
-        if (consentSchema.required) {
-          consentSchema.key = key
-          output = [...output, consentSchema]
+      let output: GigyaSdkConsentSchemaType[] = []
+
+      Object.entries(currentSiteConsents?.preferences ?? {}).forEach((entry) => {
+        const consentStatementSchema = completeGigyaSchema?.preferencesSchema?.fields[entry[0]]
+        if (entry[1]?.isActive && entry[1]?.isMandatory && consentStatementSchema) {
+          consentStatementSchema.key = entry[0]
+          output = [...output, consentStatementSchema]
         }
-      }
+      })
 
       resolve(output)
     } catch (e) {
