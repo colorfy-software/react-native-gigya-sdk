e203a473d733d2822f875bfe34f7eb0a13ca958a
Charles Mangwa <charles@colorfy.me>
2024-08-21 09:36:30 +0200

    feat: allow `setAccountInfo()` to be used during registration

--- src/core/setAccountInfo.ts
+++ src/core/setAccountInfo.ts
@@ -1,14 +1,30 @@
 import type { GigyaSdkApiResponseType } from '../types'
 
+import getState from './getState'
 import sendApiCall from './sendApiCall'
 
-export default function <DataType>(data: DataType): Promise<GigyaSdkApiResponseType> {
+type OptionsType = {
+  UID?: string
+  noUID?: boolean
+  regToken?: string
+}
+
+export default function <DataType>(data: DataType, options?: OptionsType): Promise<GigyaSdkApiResponseType> {
   return new Promise(async (resolve, reject) => {
     try {
-      const response = await sendApiCall<GigyaSdkApiResponseType, Record<keyof DataType, unknown>>(
-        'accounts.setAccountInfo',
-        data
-      )
+      const state = await getState()
+      const UID = options?.UID || state.UID
+
+      const response = await sendApiCall<
+        GigyaSdkApiResponseType,
+        Record<keyof DataType, unknown> | Omit<OptionsType, 'noUID'>
+      >('accounts.setAccountInfo', {
+        ...(options?.noUID && {
+          regToken: options?.regToken || state.regToken?.value,
+        }),
+        ...(!options?.noUID && UID && { UID }),
+        ...(data ?? {}),
+      })
 
       resolve(response)
     } catch (e) {
