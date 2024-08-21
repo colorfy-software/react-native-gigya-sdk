b8147d9e15305d063d90490a401de42cbefe93f9
Charles Mangwa <charles@colorfy.me>
2024-08-21 09:40:42 +0200

    refactor: update `getUnacceptedConsentSchemas()` types signatures

--- src/core/getUnacceptedConsentSchemas.ts
+++ src/core/getUnacceptedConsentSchemas.ts
@@ -1,37 +1,40 @@
 import getAccountInfo from './getAccountInfo'
 import getRequiredConsentSchemas from './getRequiredConsentSchemas'
 
-export type PreferencesType = {
-  [key: string]: { isConsentGranted: boolean } & {
-    [key: string]: {
-      isConsentGranted: boolean
-    }
-  }
-}
+export type PreferencesType<ConsentType extends string = string, ConsentId extends string = string> = Record<
+  ConsentType,
+  | Record<'isConsentGranted', boolean>
+  | Record<
+      ConsentId,
+      {
+        isConsentGranted: boolean
+      }
+    >
+>
 
 export default function <OutputType>(): Promise<OutputType | null> {
   return new Promise(async (resolve, reject) => {
     try {
       const requiredConsentSchemas = await getRequiredConsentSchemas()
-      const consentSchemaKeys = requiredConsentSchemas.map((schema) => schema.key)
+      const consentIds = requiredConsentSchemas.map((schema) => schema.key)
 
-      const preferences = (await getAccountInfo({ noUID: true })).preferences as PreferencesType
+      const preferences = (await getAccountInfo({ noUID: true }))?.preferences as PreferencesType | undefined
 
-      if (consentSchemaKeys.length < 1 || !preferences) {
-        return resolve(null)
-      }
+      if (consentIds.length < 1 || !preferences) return resolve(null)
 
       let output: string[] = []
 
-      for (let key of consentSchemaKeys) {
-        const [consentPreference, consentPreferenceKey] = key.split('.')
+      consentIds.forEach((consentId) => {
+        const [consentTypeOrId, subConsentId] = consentId.split('.')
+
         if (
-          !preferences?.[key]?.isConsentGranted &&
-          !preferences?.[consentPreference]?.[consentPreferenceKey]?.isConsentGranted
+          !preferences?.[consentId]?.isConsentGranted &&
+          !(preferences?.[consentTypeOrId] as Omit<PreferencesType, 'isConsentGranted'> | undefined)?.[subConsentId]
+            ?.isConsentGranted
         ) {
-          output = [...output, key]
+          output = [...output, consentId]
         }
-      }
+      })
 
       if (!output.length) {
         return resolve(null)
