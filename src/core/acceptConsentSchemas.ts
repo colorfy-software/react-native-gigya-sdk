83b0d062fc166ae26cc2a3eacf21b7d3012dbff7
Charles Mangwa <charles@colorfy.me>
2024-08-21 09:43:35 +0200

    refactor: simplify `acceptConsentSchemas()` logic

--- src/core/acceptConsentSchemas.ts
+++ src/core/acceptConsentSchemas.ts
@@ -1,3 +1,5 @@
+import type { PreferencesType } from './getUnacceptedConsentSchemas'
+
 import getState from './getState'
 import sendApiCall from './sendApiCall'
 
@@ -13,44 +15,32 @@ type OptionsType = {
   regToken?: string
 }
 
-export default function <OutputType, ConsentSchemasKeyType extends string[] = string[]>(
-  consentSchemaKeys: ConsentSchemasKeyType,
+export default function <OutputType, ConsentIdsType extends string[] = string[]>(
+  consentIds: ConsentIdsType,
   options?: OptionsType
 ): Promise<OutputType> {
   return new Promise(async (resolve, reject) => {
     try {
-      if (consentSchemaKeys.length < 1) {
+      if (consentIds.length < 1) {
         return reject(new Error('No consent schema provided'))
       }
 
       const state = await getState()
 
-      const preferences: Record<string, Record<string, unknown>> = {}
+      const preferences: PreferencesType = {} as PreferencesType
 
-      for (let key of consentSchemaKeys) {
-        const keyParts = key.split('.')
-        let objectBuild = {}
+      consentIds.forEach((id) => {
+        const [consentTypeOrId, subConsentId] = id.split('.')
 
-        for (let index = keyParts.length - 1; index >= 0; index--) {
-          const currentKey = keyParts[index]
-          let addedValue = objectBuild
-
-          if (index === keyParts.length - 1) {
-            addedValue = { isConsentGranted: true }
+        if (consentTypeOrId && subConsentId) {
+          preferences[consentTypeOrId] = {
+            ...(preferences[consentTypeOrId] ?? {}),
+            [subConsentId]: { isConsentGranted: true },
           }
-
-          if (index === 0) {
-            preferences[currentKey] = {
-              ...(preferences[currentKey] ?? {}),
-              ...addedValue,
-            }
-          } else {
-            const tempObjectBuild: Record<string, unknown> = {}
-            tempObjectBuild[currentKey] = addedValue
-            objectBuild = tempObjectBuild
-          }
+        } else if (consentTypeOrId && !subConsentId) {
+          preferences[consentTypeOrId] = { isConsentGranted: true }
         }
-      }
+      })
 
       const response = await sendApiCall<OutputType, ApiCallParamsType>('accounts.setAccountInfo', {
         ...(options?.noUID && {
